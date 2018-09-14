e6fecb304ddfdf71db2926f0283ffb6ce97eb45c
Jeff S <Slater.Jeff7@gmail.com>
2018-09-14 15:23:07 -0600

    client_response route

--- controller/client_response.js
+++ controller/client_response.js
@@ -1,12 +1,20 @@
 const model = require('../model/client_response.js')
 
 function postResponse(req, res, next) {
-  model.postResponse(req.body.client_id, req.body.question_id, req.body.answer)
+  model.postResponse(req.body.client_id, req.body.question_id, req.body.answer, req.body.score)
     .then(data => {
       console.log(data);
     })
 }
 
+function getAll(req, res, next) {
+  model.getAll()
+  .then(data => {
+    res.json(data)
+  })
+}
+
 module.exports = {
-  postResponse
+  postResponse,
+  getAll
 }
--- routes/client_response.js
+++ routes/client_response.js
@@ -5,6 +5,7 @@ const controller = require('../controller/client_response.js')
 
 
 router.post('/', controller.postResponse) // Submits a users's response for a question.
+router.get('/', controller.getAll)//gets all
 
 
 module.exports = router
